06dc35c030b5769ba8ab894eadd4ecf59cd14e0b
Sm. Asfakur Rahman Saikat <saikat.hap@gmail.com>
2023-11-18 07:34:43 +0600

    2 inheritance

---
+++ 4_inheritance/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        TextBox control = new TextBox();
+        control.disable();
+        System.out.println(control.isEnabled());
+    }
+}
